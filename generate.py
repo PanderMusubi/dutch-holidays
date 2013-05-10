0dbb6cb8945f1eb49f88eaed93ed93074d8280f8
Pander <pander@users.sourceforge.net>
2013-05-10 15:22:44 +0200

    First testing version

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ License		...
 Initial release
 """
 
-import os
+import datetime, os
 
 kalender = open('NederlandseFeestdagen.ics', 'w')
 calendar = open('DutchHolidays.ics', 'w')
@@ -40,8 +40,33 @@ for holiday_file in sorted(os.listdir(directory)):
         for line in holiday.readlines():
             kalender.write(line)
             calendar.write(line)
-        kalender.write(holiday_footer)
-        calendar.write(holiday_footer)
+        if 'Nieuwjaarsdag' in naam or 'Goede Vrijdag' in naam or 'Paasdag' in naam or 'Koning' in naam or 'Bevrijdinsdag' in naam or 'Hemelvaartsdag' in naam or 'Pinksterdag' in naam or 'Kerstdag' in naam:
+            kalender.write(holiday_footer)
+            calendar.write(holiday_footer)
+        else:
+            kalender.write(holiday_footer.replace('Public Holiday', 'Unofficial Public Holiday'))
+            calendar.write(holiday_footer.replace('Public Holiday', 'Unofficial Public Holiday'))
+
+directory = 'unscripted-holidays'
+for holiday_file in sorted(os.listdir(directory)):
+    if holiday_file.endswith(".txt"):
+        holiday = open(directory+ '/' + holiday_file,'r')
+        (naam, name) = holiday_file[:-4].split('_')
+        for line in holiday.readlines():
+            kalender.write(holiday_header.strip()+naam+'\n')
+            calendar.write(holiday_header.strip()+naam+' ('+name+')\n')
+            date = datetime.datetime.strptime(line.strip(), '%Y%m%d')
+            kalender.write('DTSTART;VALUE=DATE:'+date.strftime('%Y%m%d')+'\n')
+            calendar.write('DTSTART;VALUE=DATE:'+date.strftime('%Y%m%d')+'\n')
+            date += datetime.timedelta(days=1)
+            kalender.write('DTEND;VALUE=DATE:'+date.strftime('%Y%m%d')+'\n')
+            calendar.write('DTEND;VALUE=DATE:'+date.strftime('%Y%m%d')+'\n')
+            if 'Nieuwjaarsdag' in naam or 'Goede Vrijdag' in naam or 'Paasdag' in naam or 'Koning' in naam or 'Bevrijdinsdag' in naam or 'Hemelvaartsdag' in naam or 'Pinksterdag' in naam or 'Kerstdag' in naam:
+                kalender.write(holiday_footer)
+                calendar.write(holiday_footer)
+            else:
+                kalender.write(holiday_footer.replace('Public Holiday', 'Unofficial Public Holiday'))
+                calendar.write(holiday_footer.replace('Public Holiday', 'Unofficial Public Holiday'))
 
 calendar_footer= open('calendar-footer.txt', 'r')
 for line in calendar_footer.readlines():
