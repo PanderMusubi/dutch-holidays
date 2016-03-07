615d6da52a1ac0bb9a7b0d1797e8e6892cbed38b
Pander <pander@users.sourceforge.net>
2016-03-07 14:08:03 +0100

    Fixed typo in date and improved new lines

--- post-process.sh
+++ post-process.sh
@@ -1,3 +1,3 @@
+flip -m *.ics
 grep SUMMARY NederlandseFeestdagen.ics|sed -e 's/SUMMARY://'|sort|uniq>names-used-dutch.txt
 grep SUMMARY DutchHolidays.ics|sed -e 's/SUMMARY://'|sort|uniq>names-used-english.txt
-
