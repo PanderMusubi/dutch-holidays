f5a88800cb76e4ea940d67274e241ea2dcffd4d8
Pander <pander@users.sourceforge.net>
2016-11-07 23:43:34 +0100

    Improved reporting

--- report.sh
+++ report.sh
@@ -1,15 +1,13 @@
-rm -f report.txt
+echo -e 'Dutch holidays:'
+grep SUMMARY NederlandseFeestdagen.ics|sed -e 's/SUMMARY://'|sort|uniq 
+echo -e '\nEnglish translations of Dutch holidays:'
+grep SUMMARY DutchHolidays.ics|sed -e 's/SUMMARY://'|sort|uniq 
+echo -e '\nGerman translation of Dutch holidays:'
+grep SUMMARY NiederlaendischeFeiertage.ics|sed -e 's/SUMMARY://'|sort|uniq 
 
-echo -e 'Dutch holidays:'>>report.txt
-grep SUMMARY NederlandseFeestdagen.ics|sed -e 's/SUMMARY://'|sort|uniq >>report.txt
-echo -e '\nEnglish translations of Dutch holidays:'>>report.txt
-grep SUMMARY DutchHolidays.ics|sed -e 's/SUMMARY://'|sort|uniq >>report.txt
-echo -e '\nGerman translation of Dutch holidays:'>>report.txt
-grep SUMMARY NiederlaendischeFeiertage.ics|sed -e 's/SUMMARY://'|sort|uniq >>report.txt
-
-echo -e '\n\nDutch categories:'>>report.txt
-grep CATEGORIES NederlandseFeestdagen.ics|sed -e 's/CATEGORIES://'|sort|uniq >>report.txt
-echo -e '\nEnglish translations of Dutch categories:'>>report.txt
-grep CATEGORIES DutchHolidays.ics|sed -e 's/CATEGORIES://'|sort|uniq >>report.txt
-echo -e '\nGerman translations of Dutch categories:'>>report.txt
-grep CATEGORIES NiederlaendischeFeiertage.ics|sed -e 's/CATEGORIES://'|sort|uniq >>report.txt
+echo -e '\n\nDutch categories:'
+grep CATEGORIES NederlandseFeestdagen.ics|sed -e 's/CATEGORIES://'|sort|uniq 
+echo -e '\nEnglish translations of Dutch categories:'
+grep CATEGORIES DutchHolidays.ics|sed -e 's/CATEGORIES://'|sort|uniq 
+echo -e '\nGerman translations of Dutch categories:'
+grep CATEGORIES NiederlaendischeFeiertage.ics|sed -e 's/CATEGORIES://'|sort|uniq 
